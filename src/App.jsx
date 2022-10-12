35769fb64a796ec46d54d3aa27448c2ef8e89a4f
manu <manuelibanezastorga@gmail.com>
2022-10-12 12:13:07 -0300

    agregamos el spineer mientras carga la card de los personajes de los simposns

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 import Frase from "./components/Frase";
-import Spinner from "./components/Spinner"
+import Spinner from "./components/Spinner";
 import { Button, Container } from "react-bootstrap";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(() => {
     consultaApi();
   }, []);
+
   const consultaApi = async () => {
     //la consulta de la api solo sucede en el montaje
     try {
+      setMostrarSpinner(true)
       const respuesta = await fetch(
         "https://thesimpsonsquoteapi.glitch.me/quotes"
       );
@@ -22,11 +25,22 @@ function App() {
       console.log(dato[0]);
       //guardar la frase del personaje en el state
       setPersonaje(dato[0]);
+      setMostrarSpinner(false)
     } catch (error) {
       //mostrar unmensaje de error al usuario
       console.log(error);
     }
   };
+  //cargar un componente comdicional
+  //usamos el operador ternario
+  //(condicion logica) ?(codigo si se umole la condicion logica):(logica si no se cumplela condicion)
+  //const componenteCondicional = (mostrarSpinner)?():()
+  const componenteCondicional =
+    mostrarSpinner === true ? (
+      <Spinner></Spinner>
+    ) : (
+      <Frase personaje={personaje}></Frase>
+    );
 
   return (
     <Container>
@@ -39,13 +53,16 @@ function App() {
             alt="Los simpsons titulo"
           />
         </div>
-        <Button type="button" variant="warning" className="w-50 mb-5" onClick={consultaApi} >
+        <Button
+          type="button"
+          variant={"warning"}
+          className="w-50 mb-5"
+          onClick={consultaApi}
+        >
           Obtener frase
         </Button>
+        {componenteCondicional}
       </div>
-      
-        <Frase personaje={personaje}></Frase>
-      <Spinner></Spinner>
     </Container>
   );
 }
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,13 +2,16 @@ import './spinner.css'
 
 const Spinner = () => {
   return (
-    <div>
-      <div ClassName="spinner">
-        <div className="double-bounce1"></div>
-        <div className="double-bounce2"></div>
+    <div className='d-flex justify-content-center'>
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
       </div>
     </div>
   );
 };
-
 export default Spinner;
